fa7d6a735d1bce31d96938089d7913880a175743
LucyVeron <lucymveron@gmail.com>
2024-10-22 13:27:17 +0200

    fetch movie by ID

--- movies/src/main/java/com/fiercebrosnan/movies/MovieController.java
+++ movies/src/main/java/com/fiercebrosnan/movies/MovieController.java
@@ -1,11 +1,13 @@
 package com.fiercebrosnan.movies;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(service.findMovieById(imdbId), HttpStatus.OK);
     }
 }
